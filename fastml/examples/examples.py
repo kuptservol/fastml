479671bfb3fcb06f59b83fd03e03eda418950a17
Sergey Kuptsov <kuptservol@yandex-team.ru>
2019-11-10 18:39:36 +0300

    * general relu

--- fastml/examples/examples.py
+++ fastml/examples/examples.py
@@ -15,10 +15,10 @@ def get_simple_mnist_lin_learner(data, lr=0.3, nh=50):
 def get_mnist_cnn(mnist_data, lr=0.3, cnn_layers=[8,16,32,32], loss=loss.cross_entropy(), set_opt_F=lambda model, lr: optimizers.SGD(model, lr), cb_funcs=[]):
 
     mnist_view = view_tfm(1,28,28)
-    [cb_funcs.append(cb) for cb in [Recorder, partial(AvgStatsCallback, metrics.accuracy), partial(BatchTransformXCallback, mnist_view)]]
+    cbfs = cb_funcs+[Recorder, partial(AvgStatsCallback, metrics.accuracy), partial(BatchTransformXCallback, mnist_view)]
     model = get_cnn_model(mnist_data, cnn_layers)
     opt = set_opt_F(model, lr)
     learner =  Learner(model, opt, loss, mnist_data)
-    runner = Runner(cb_funcs=cb_funcs)
+    runner = Runner(cb_funcs=cbfs)
 
     return runner, learner
--- fastml/model/image/__init__.py
+++ fastml/model/image/__init__.py
@@ -2,3 +2,4 @@ from fastml.model.init import *
 from fastml.model.loss import *
 from fastml.data import *
 from fastml.core import *
+from fastml.model.model import *
